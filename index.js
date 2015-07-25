9aed3f12d09908aa108653fe9a69f6281de295bc
Kyle <pottsk@purdue.edu>
2015-07-25 17:56:49 -0400

    add now

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ io.on('connection',function(socket){
 	socket.on('play',function(){
 		log.info('user pressed play');
 		var now =moment();
-		io.emit('play',{time:now});
+		io.emit('play',{time:now.add(100,'milliseconds')});
 		log.info('after emit');
 	});
 
